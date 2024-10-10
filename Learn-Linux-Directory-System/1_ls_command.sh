f7b4be7cd09d3bff1514df1cb7d2f01dbc55a37a
istiakAHMEDsaad <istik2020@gmail.com>
2024-10-10 23:29:44 +0600

    add more notes

--- Learn-Linux-Directory-System/1_ls_command.sh
+++ Learn-Linux-Directory-System/1_ls_command.sh
@@ -1,16 +1,39 @@
 #ls [option] [fd] --{file directory}
+
 #Example =>
+
 #1. ls Downloads/ --{it's will show list item for this specific folder}
+
 #2. pwd --{present working directory}
-#3. ls ~ --{show root directory}
-#4. ls .. --{show 1 step backward from current directory and list them}
-#5. ls ../.. --{show the root directory}
-#6. ls -l --{list item long format}
+
+#3. ls ~ 	--{show root directory}
+
+#4. ls .. 	--{show 1 step backward from current directory and list them}
+
+#5. ls ../.. 	--{show the root directory}
+
+#6. ls -l 	--{list item long format}
+
 #7. Mode in linux 
 #	[d] -{directory}
 #	[-] --{not a directory}
 #	[r] --{read}
 #	[w] --{wirte}
 #	[x] --{execute}
+
 #format is {drwxrwxrwx} / {-rwxrwxrwx}
-#8. ls -a
+
+#8. ls -a 	--{show all hidden file}
+
+#9. ls -al  	--{show hidden file along long list}
+
+#10. ls -lS  	--{sort directory by size}
+
+#11. ls Documents/*.sh 	--{in documents directory show only sh extension file}
+
+#12. ls Documents/*.*	--{show all file in directory documents}
+
+#13. ls -lS > output.txt	--{the command results value will expot in output.txt file if the file is not exist file will be create auto}
+
+#14. ls -d */	--{it'll list all directory}
+
