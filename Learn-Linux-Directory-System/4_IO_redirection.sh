00a5bde5b1eb093dc090a72a30bb1faff7cd7161
istiakAHMEDsaad <istik2020@gmail.com>
2024-10-11 12:54:23 +0600

    io command

---
+++ Learn-Linux-Directory-System/4_IO_redirection.sh
@@ -0,0 +1,11 @@
+# [output] > [file name]
+
+# cat > [file-name.extension] 	--{write in text file directly [press 'Ctrl' + 'D'] to stop cat}
+
+# cat [file-name.extension] 	--{to see what inside the text file}
+
+# cat >> [file-name.extension]	--{append new text to previous file}
+
+# cat [file-name.extension] [file-name.extension] > [output-file-name.extension] 	--{this will create a new file which content file 1, file 2 data}
+
+
