093fa76e65c6d8abdd12c99e18e12b0df89eb082
istiakAHMEDsaad <istik2020@gmail.com>
2024-11-30 23:58:36 +0600

    simple practice

---
+++ Learn-Linux-Directory-System/Practice/1_1.echo_simple_command.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello World" #echo for print 
+
+#simple comman using bash
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
---
+++ Learn-Linux-Directory-System/Practice/1_2.variable_comments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="SAAD"
+echo Your name is $name
+
+echo Our bash shell name is $BASH
+echo Our bash shell version is $BASH_VERSION
+echo Our home directory is $HOME
+echo Our current path is $PWD
